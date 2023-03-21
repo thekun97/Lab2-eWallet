76003c828a1b561cc6208a115135900f04f5ad0b
thekun97 <npnam.techii@gmail.com>
2023-03-21 20:46:17 +0700

    auth

--- frontend.py
+++ frontend.py
@@ -1,23 +1,27 @@
 from typing import Dict
-
+import uuid
 from fastapi import FastAPI
-from nicegui import ui
+from fastapi import Request
+from nicegui import app, ui
+from fastapi.responses import RedirectResponse
 
 from create_account import create_account
 from create_account import decrypt_private_key
+from starlette.middleware.sessions import SessionMiddleware
 
 session_info: Dict[str, Dict] = {}
 
+app.add_middleware(SessionMiddleware, secret_key='some_random_string')
+
 
 def init(app: FastAPI) -> None:
+    def is_authenticated(request: Request) -> bool:
+        return session_info.get('user', {}).get('authenticated', False)
+
     @ui.page('/')
-    def index():
-        data = None
-        if 'user-data' in session_info:
-            data = session_info['user-data']
-            print(data)
-            if data["authenticated"]:
-                ui.open('/my-wallet')
+    def index(request: Request):
+        if is_authenticated(request):
+            return RedirectResponse('/my-wallet')
 
         with ui.row().classes('mt-8 w-full justify-center items-center'):
             ui.label('Welcome to Kun Chain')
@@ -26,13 +30,13 @@ def init(app: FastAPI) -> None:
             ui.button('Create a new Wallet', on_click=lambda: ui.open('/create-wallet'))
 
     @ui.page('/create-wallet')
-    def create_wallet():
+    def create_wallet(request: Request):
         def handle_create_account() -> None:
             if password.value == '' or re_password.value == '' or password.value != re_password.value:
                 ui.notify('Password not match', color='negative')
             else:
                 address, _ = create_account(password.value)
-                session_info['user-data'] = {'address': address, 'authenticated': True}
+                session_info['user'] = {'address': address, 'authenticated': True}
                 ui.open('/my-wallet')
 
         with ui.row().classes('mt-8 w-full justify-center items-center'):
@@ -61,7 +65,7 @@ def init(app: FastAPI) -> None:
             session_info['private_key'] = {'private_key': private_key}
             show(private_key)
 
-        data = session_info['user-data']
+        data = session_info['user']
         with ui.dialog() as dialog, ui.card().style('width: 700em'):
             with ui.row().classes('w-full justify-center items-center column'):
                 password = ui.input('Enter your password').props('type=password')
